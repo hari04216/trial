e6c642da1890c2708c0c04b42b12d142a37f77a0
dell <dell@krish>
2023-04-04 12:34:03 +0530

    trial sprint1

---
+++ src/test/java/tgithub/test2.java
@@ -0,0 +1,17 @@
+package tgithub;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+
+	@Test
+
+	public void login() {
+		System.out.println("hi");
+	}
+
+	public void logout() {
+		System.out.println("bi");
+	}
+
+}
